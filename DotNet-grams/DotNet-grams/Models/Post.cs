f72ae6b1b5c3557a7ea9c0342c1741a7d0631e6f
xia LIU <xia.liu2@outlook.com>
2019-02-13 11:16:51 -0800

    added dbcontext and start working on services for the interfaces

---
+++ DotNet-grams/DotNet-grams/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet_grams.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string PosterName { get; set; }
+        public string Description { get; set; }
+        public string URL { get; set; }
+    }
+}
